36ac8dce9363b8fe203292b33f80b7cb8700e147
Emerick Bosch <xpc.dev@gmail.com>
2025-09-01 22:25:45 +0200

    Move request-line models to its file

--- src/http/models.go
+++ src/http/models.go
@@ -7,35 +7,3 @@ type Request struct {
 }
 
 type URI string
-
-type HttpMethod string
-
-const (
-	Get    HttpMethod = "GET"
-	Post   HttpMethod = "POST"
-	Put    HttpMethod = "PUT"
-	Delete HttpMethod = "Delete"
-	Option HttpMethod = "Option"
-)
-
-func (m HttpMethod) IsValid() bool {
-	switch m {
-	case Get, Post, Put, Delete, Option:
-		return true
-	}
-	return false
-}
-
-type HttpVersion string
-
-const (
-	OneOne HttpVersion = "HTTP/1.1"
-)
-
-func (m HttpVersion) IsValid() bool {
-	switch m {
-	case OneOne:
-		return true
-	}
-	return false
-}
--- src/http/request_line.go
+++ src/http/request_line.go
@@ -11,6 +11,38 @@ type RequestLine struct {
 	Version    HttpVersion
 }
 
+type HttpMethod string
+
+const (
+	Get    HttpMethod = "GET"
+	Post   HttpMethod = "POST"
+	Put    HttpMethod = "PUT"
+	Delete HttpMethod = "Delete"
+	Option HttpMethod = "Option"
+)
+
+func (m HttpMethod) IsValid() bool {
+	switch m {
+	case Get, Post, Put, Delete, Option:
+		return true
+	}
+	return false
+}
+
+type HttpVersion string
+
+const (
+	OneOne HttpVersion = "HTTP/1.1"
+)
+
+func (m HttpVersion) IsValid() bool {
+	switch m {
+	case OneOne:
+		return true
+	}
+	return false
+}
+
 func ParseRequestLine(input string) (RequestLine, error) {
 	requestLine := RequestLine{}
 
